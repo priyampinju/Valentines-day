0961ea03024d73cf429590b8f26eef48122d86ed
priyampinju <pinjupriyam1826@gmail.com>
2024-02-08 01:59:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const phrases = [
+  "No",
+  "Are you sure?",
+  "Really sure",
+  "Please 😭",
+  "Don't do this to me",
+  "I'm gonna cry...",
+  "You're breaking my heart 😭",
+  "I'm asking for the last time",
+  "Please say yes 😭",
+]
+
+const App = () => {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  function handleNoClick() {
+    setNoCount(noCount + 1);
+  }
+
+  function getNoButtonText() {
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='valentine-container'>
+      {yesPressed ? (
+        <div className='yes-pressed'>
+          <div class="tenor-gif-embed" data-postid="18274294" data-share-method="host" data-aspect-ratio="1.20755" data-width="100%"></div>
+          <div className="text">Yayy!!!</div>
+        </div>
+      ) : (
+        <div className='not-pressed'>
+          <div class="tenor-gif-embed" data-postid="13498319440014303012" data-share-method="host" data-aspect-ratio="1" data-width="100%"></div>
+          <div className='text'>
+            Will you be my valentine?
+          </div>
+          <div className='button-wrapper'>
+            <button className="yesButton" style={{ fontSize: yesButtonSize }} onClick={() => setYesPressed(true)}>
+              Yes
+            </button>
+            <button className="noButton" onClick={handleNoClick}>
+              {getNoButtonText()}
+            </button>
+          </div>
+        </div>
+
+
+
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
